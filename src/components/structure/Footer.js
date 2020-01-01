b210a1bc815bf35d490d04ca137170e7ad8d1c3a
flash <sachinkotian007@yahoo.com>
2020-01-01 21:54:55 +0530

    initializing

---
+++ src/components/structure/Footer.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer className="footer has-background-link">
+            <div className="content has-text-centered has-text-white">
+                <p>
+                    Built by <i className="fas fa-moon"></i> by <strong className="has-text-white"><a className="has-text-white shine" href="https://github.com/flashnoob">Sachin Kotian</a></strong>
+                </p>
+               
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
