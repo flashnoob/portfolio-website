d11e11b3ac3f9e906a2dd841a95a65b33430dc34
flash <sachinkotian007@yahoo.com>
2020-01-02 18:00:29 +0530

    Updates

---
+++ precache-manifest.313a2f97cd0cfdcaf786ad6c295578ec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e1e731819752b5e1982",
+    "url": "/portfolio-website/static/js/runtime~main.7784a2cd.js"
+  },
+  {
+    "revision": "1d17ccf2152133ec1bd0",
+    "url": "/portfolio-website/static/js/main.32dde7ce.chunk.js"
+  },
+  {
+    "revision": "c9f7bc571661205fe9d1",
+    "url": "/portfolio-website/static/js/2.ce8a9bb8.chunk.js"
+  },
+  {
+    "revision": "6821b7e459bccaff78a2519818906b35",
+    "url": "/portfolio-website/index.html"
+  }
+];
